3862c0e73631124d0af9300ad2b9fbd0a0f75b9d
Joao Pedro <joaopedro0498@hotmail.com>
2022-02-06 23:06:42 -0300

    Added the / GET route to show available registers

--- app.js
+++ app.js
@@ -14,3 +14,22 @@ app.use(express.urlencoded({extended: false})); // Get parameters from url
 app.listen(PORT, () => {
     console.log('Server running on: http://localhost:3000');
 });
+
+// (/) -> Get a general view of the database registers
+app.get('/', (req, res) => {
+
+    var SQL = "SELECT * FROM livros;";
+
+    db.all(SQL, [], (err, rows) => {
+        // If the callback function returns some error it show it on the log.
+        if (err) {
+            console.error(err.message);
+        }
+
+        // To pass data to use with the EJS we use the res.render method it kind
+         // of create a scope to use with the EJS scriptlets with the passed
+         // JSON or collection
+
+        res.render("index.ejs" , { dados: rows });
+    });
+});
