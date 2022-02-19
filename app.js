0bce48bd63ae9b63501935b16544d60518ed847d
Joao Pedro <joaopedro0498@hotmail.com>
2022-02-19 12:09:14 -0300

    Fixing typos

--- app.js
+++ app.js
@@ -2,23 +2,23 @@ const express = require('express');
 const app = express(); // Express boilerplate
 
 const sqlite = require("sqlite3");
-const db = new sqlite.Database("./bd_livraria.db");
+let db = new sqlite.Database("./bd_livraria.db");
 
 const PORT = 3000;
 
-app.set('engine ejs', 'ejs'); // Setting up node app to use ejs pages over html
+app.set("engine ejs", "ejs"); // Setting up node app to use ejs pages over html
 
 // Middleware
-app.use(express.urlencoded({extended: false})); // Get parameters from url
+app.use(express.urlencoded({extended: false})); // To get form data from req.body
 
 app.listen(PORT, () => {
-    console.log('Server running on: http://localhost:3000');
+    console.log(`Server running on: http://localhost:${PORT}`);
 });
 
-// (/) -> Get a general view of the database registers
+// "/" route -> Get a general view of the database registers
 app.get('/', (req, res) => {
 
-    var SQL = "SELECT * FROM livros ORDER BY id DESC;";
+    const SQL = "SELECT * FROM livros ORDER BY id DESC;";
 
     db.all(SQL, [], (err, rows) => {
         // If the callback function returns some error it show it on the log.
@@ -26,9 +26,9 @@ app.get('/', (req, res) => {
             console.error(err.message);
         }
 
-        // To pass data to use with the EJS we use the res.render method it kind
-         // of create a scope to use with the EJS scriptlets with the passed
-         // JSON or collection
+        // To pass data to use with the EJS template we use the res.render
+        // method. It kind of create a scope to use with the EJS scriptlets with
+        // the passed JSON or collection
 
         res.render("index.ejs" , { data: rows });
     });
