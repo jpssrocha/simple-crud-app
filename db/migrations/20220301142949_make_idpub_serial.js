d83a0ff8c0078b75f7a314efd28c8bc6c46f332c
Joao Pedro <joaopedro0498@hotmail.com>
2022-03-01 11:50:18 -0300

    Added migration to make idpub serial

---
+++ db/migrations/20220301142949_make_idpub_serial.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.table("publicacao", (table) => {
+        table.dropColumn("idpub");
+        table.increments("id_pub");
+    });
+
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.table("publicacao", (table) => {
+        table.dropColumn("id_pub");
+        table.integer("idpub").notNullable();
+    });
+
+};
