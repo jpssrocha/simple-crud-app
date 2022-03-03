16054adeea5e3ffb781847d55ef1c90a4d04a4d1
Joao Pedro <joaopedro0498@hotmail.com>
2022-03-03 11:21:49 -0300

    added cascading

--- db/migrations/20220301142949_make_idpub_serial.js
+++ db/migrations/20220301142949_make_idpub_serial.js
@@ -5,6 +5,8 @@
 exports.up = function(knex) {
     return knex.schema.table("publicacao", (table) => {
         table.dropColumn("idpub");
+        table.dropForeign("livro_id");
+        table.foreign("livro_id").references("livros.id").onDelete("CASCADE");
         table.increments("id_pub");
     });
 
@@ -17,6 +19,8 @@ exports.up = function(knex) {
 exports.down = function(knex) {
     return knex.schema.table("publicacao", (table) => {
         table.dropColumn("id_pub");
+        table.dropForeign("livro_id");
+        table.foreign("livro_id").references("livros.id");
         table.integer("idpub").notNullable();
     });
 
