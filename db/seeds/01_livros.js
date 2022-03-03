1e51399b3a6f0d2186064c5381c74549dca6f603
Joao Pedro <joaopedro0498@hotmail.com>
2022-03-03 15:00:07 -0300

    final modification to seeds

--- db/seeds/01_livros.js
+++ db/seeds/01_livros.js
@@ -6,11 +6,13 @@ const fs = require('fs');
 const { join } = require('path');
 const { parse } = require('csv-parse/sync');
 
-const fileContent = fs.readFileSync(join(__dirname, 'bd_livraria-livros.csv'));
+const fileContent = fs.readFileSync(join(__dirname, 'bd_livraria-livros-serialized.csv'));
 const data = parse(fileContent, {columns: true});
 
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
-  await knex('livros').del()
+  await knex('livros').del();
   await knex('livros').insert(data);
+  await knex.raw(`SELECT setval('livros_id_seq', (SELECT MAX(id) FROM livros)+1);`);
+
 };
