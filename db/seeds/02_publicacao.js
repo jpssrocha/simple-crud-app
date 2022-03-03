cde20a1d784024bb3d6e06ea7d86c61cf79de9aa
Joao Pedro <joaopedro0498@hotmail.com>
2022-03-03 15:12:36 -0300

    Revert "final modification to seeds"
    This reverts commit 1e51399b3a6f0d2186064c5381c74549dca6f603.

--- db/seeds/02_publicacao.js
+++ db/seeds/02_publicacao.js
@@ -5,12 +5,11 @@
 const fs = require('fs');
 const { parse } = require('csv-parse/sync');
 
-const fileContent = fs.readFileSync(__dirname + '/bd_livraria-publicacao-serialized.csv');
+const fileContent = fs.readFileSync(__dirname + '/bd_livraria-publicacao.csv');
 const data = parse(fileContent, {columns: true});
 
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('publicacao').del()
   await knex('publicacao').insert(data);
-  await knex.raw(`SELECT setval('publicacao_id_pub_seq', (SELECT MAX(livro_id) FROM publicacao)+1);`);
 };
